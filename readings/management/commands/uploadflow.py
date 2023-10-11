0ff3ce1584ee9d971c98e1c141d63c0e6c468100
King-EL <69638147+King-EL@users.noreply.github.com>
2023-10-11 16:37:43 +0100

    finalising custom command. Creation of database obj now included and feedback in the terminal on successful/non-successful upload.

--- readings/management/commands/uploadflow.py
+++ readings/management/commands/uploadflow.py
@@ -1,7 +1,8 @@
 import argparse
-
+from datetime import datetime
 from django.core.management import BaseCommand, CommandError
 
+from readings.models import Reading
 
 class Command(BaseCommand):
     # shown in terminal when user types --help
@@ -13,21 +14,38 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         sections = []
         current_section = {}
+        uff_file_name = options["uff"]
+        uff = options["uff"].read()
 
         # split file content into lines
-        lines = options["uff"].split("\n")
+        lines = uff.split("\n")
         for line in lines:
-            # split each line into fields based on the "|" delimiter
+            # split each lin into fields based on '|' delimiter
             fields = line.split("|")
 
-            # assess current section and add it to sections
+            #  assess current section and add it to sections
             if fields[0] == "026":
                 current_section = {"026": fields[1]}
             elif fields[0] == "028":
-                current_section = {"028": fields[1]}
+                current_section["028"] = fields[1]
             elif fields[0] == "030":
-                # end section add it to sections
-                current_section = {"reading": fields[3], "date": fields[2]}
+                # end of section add it to sections
+                current_section["030"] = fields[3], fields[2]
                 sections.append(current_section)
 
-        print(sections)
+        for section in sections:
+            try:
+                date_str = section["030"][1]
+                date_format = "%Y%m%d%H%M%S"
+                date_obj = datetime.strptime(date_str, date_format)
+                obj, created = Reading.objects.get_or_create(
+                    flow_file_name = uff_file_name,
+                    meter_point_reference_number = section.get("026", [0]),
+                    meter_serial_number = section.get("028", [0]),
+                    reading = section.get("030")[0],
+                    reading_date = date_obj
+                )
+                if created:
+                    self.stdout.write(f"{uff_file_name} successfully created")
+            except Exception as e:
+                self.stderr.write(f"Error processing file: {str(e)}")
