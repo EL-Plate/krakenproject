2030faf34c25eabc4c638158ba0b4fe7bb4c67c1
King-EL <69638147+King-EL@users.noreply.github.com>
2023-10-11 14:19:06 +0100

    Correcting mistake in settings.py. Changes to readings file structure to include files for custom management command.

--- krakenproject/settings.py
+++ krakenproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'readings.apps.ReadingsConfig'
+    'readings.apps.ReadingsConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
