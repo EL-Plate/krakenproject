6c65d8746a1382e78289e76c36c404a7d7c8a2b4
King-EL <69638147+King-EL@users.noreply.github.com>
2023-10-11 14:01:31 +0100

    Creation of Reading class and associated __str__ method in model.py

--- readings/models.py
+++ readings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Reading(models.Model):
+    reading_id = models.AutoField(primary_key=True)
+    flow_file_name = models.CharField(max_length=200)
+    meter_point_reference_number = models.CharField(max_length=20)
+    meter_point_serial_number = models.CharField(max_length=20)
+    reading = models.FloatField(max_length=5)
+    reading_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.meter_point_reference_number, self.meter_point_serial_number, self.reading, self.reading_date}"
